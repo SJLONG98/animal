b1fb3a1169dec191c3408ea32bf1b217316b8e8d
SJLONG98 <63651516+SJLONG98@users.noreply.github.com>
2020-04-16 11:23:23 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-	$db = new PDO("mysql:dbname=neatejm_db;host=localhost","neatejm","leer11prop");
+	$db = new PDO("mysql:dbname=HelpingHeroes_DB;host=localhost","Admin","Admin");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
